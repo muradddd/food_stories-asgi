21480e598d4b342f239e419b5be8cd4c78c019ee
Murad <murad.rustemzade@gmail.com>
2020-02-02 08:09:02 +0000

    asgi settings configured

--- food_stories/asgi.py
+++ food_stories/asgi.py
@@ -7,14 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-#import os
-
-#from django.core.asgi import get_asgi_application
-
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_stories.settings')
-
-#application = get_asgi_application()
-
 import os
 import django
 from channels.routing import get_default_application
